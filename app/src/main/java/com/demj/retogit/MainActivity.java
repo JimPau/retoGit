d0bbf79150785d71231959f105a03e679880a1b5
JimePau <jpaucar789@gmail.com>
2023-02-02 07:47:59 -0500

    Se agrego una imagen y un boton al MainActivity

--- app/src/main/java/com/demj/retogit/MainActivity.java
+++ app/src/main/java/com/demj/retogit/MainActivity.java
@@ -3,8 +3,15 @@ package com.demj.retogit;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void buttonAcep(View view){
+        Toast.makeText(MainActivity.this, "Bienvenido a este nuevo mundo ☺", Toast.LENGTH_SHORT).show();
+        
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
